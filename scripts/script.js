a7521c192b0848b938e1276b015422c0594e3170
Dominik Koniorczyk <d.koniorczyk@limesgames.de>
2025-11-21 15:41:25 +0100

    finish styling of card

--- scripts/script.js
+++ scripts/script.js
@@ -9,8 +9,6 @@ async function getNextPokemon(){
     let responseToJson = await response.json();
     responseToJson.results.forEach(result => {
         let lastEl = responseToJson.results.findLast((element) => element) == result;
-        console.log(lastEl);
-        
         renderMainCard(result, lastEl);
     });
 }
@@ -18,9 +16,7 @@ async function getNextPokemon(){
 async function renderMainCard(responseData={}, lastElement=bool){
     const URL = responseData.url;
     const response = await fetch(URL);
-    let responseToJson = await response.json();
-    console.log(responseToJson.id);
-    
+    let responseToJson = await response.json();    
     pokemon.push({id: responseToJson.id, name: responseToJson.name, mainImage: responseToJson.sprites.other.home.front_default, types: [responseToJson.types], weight: responseToJson.weight, stats: responseToJson.stats});
     loadDone = lastElement;
     if(loadDone){
@@ -39,8 +35,28 @@ function sortPokemonById(){
 // #region render cards
 async function renderCardsOnPage(){
     const BODY_ELEMENT = document.getElementById('card_content'); 
-    await 
-    pokemonDataBase.forEach(actualPokemon => {
-        BODY_ELEMENT.innerHTML += returnCardTemplate(actualPokemon);
+    await pokemonDataBase.forEach(actualPokemon => {
+            let pokemonClasses = "";
+            actualPokemon.types[0].forEach(classes => {
+                pokemonClasses += returnClassImages(classes);   
+                test.push(classes.type.url) 
+            });
+        BODY_ELEMENT.innerHTML += returnCardTemplate(actualPokemon, pokemonClasses);
     });
+    Test();
+}
+
+let test = []
+
+async function Test() {
+    test.forEach((response) => {
+        FetchNew(response);
+    })
+}
+
+async function FetchNew(urlIn){
+    const response = await fetch(urlIn);
+    let nasw = await response.json();
+    console.log(nasw);
+    
 }
--- scripts/templates.js
+++ scripts/templates.js
@@ -1,17 +1,26 @@
-function returnCardTemplate(pokemonData={}, ) {
+function returnCardTemplate(pokemonData={}, classes) {
     return /*html*/`
         <article class = "card">
             <div class = "card_inner">
-                <div class="card_headline">
-                    <h2>${pokemonData.name.charAt(0).toUpperCase() + pokemonData.name.slice(1)}</h2>
+                <div class = "card_headline">
+                    <h2 class = "inner_headline">${pokemonData.name.charAt(0).toUpperCase() + pokemonData.name.slice(1)}</h2>
+                    <h2 class = "number_headline">#${pokemonData.id}</h2>
                 </div>
                 <div class="pokemon_img_container ${pokemonData.types[0][0].type.name}">
                     <img src=${pokemonData.mainImage} alt="" srcset="">
                 </div>
                 <div class="pokemon_type_container">
-                    <button class="test"></button>
+                    ${classes}
                 </div>
             </div>
         </article>
+    `;
+}
+
+function returnClassImages(data){
+    return /*html*/`
+        <button class = "class_icon ${data.type.name}">
+            <div class ="${data.type.name}_imgage" src="" alt="">
+        </button>
     `;
 }
