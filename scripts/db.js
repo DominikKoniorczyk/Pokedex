6001877a048e2b6e050e499ff4a12fd3f963e538
Dominik Koniorczyk <d.koniorczyk@limesgames.de>
2025-11-22 16:14:30 +0100

    Update function for fetching data

--- scripts/db.js
+++ scripts/db.js
@@ -2,5 +2,5 @@ let pokemon = [];
 let pokemonDataBase = [];
 let loadDone = false;
 let scrollChecking = false;
-let lastLoadedElements = "0"
+let lastLoadedElements = 0;
 const BASE_URL = "https://pokeapi.co/api/v2/pokemon?limit=50&offset=";
--- scripts/script.js
+++ scripts/script.js
@@ -15,6 +15,18 @@ function addEventListener(){
         scrollChecking = true;}
     }); 
 }
+
+function checkElementIsInView(scrollYPosition)
+{
+    if(lastLoadedElements < 500){
+        let elementsPerRow = window.innerWidth < 1440 ? Math.floor(window.innerWidth / 236) : 6;
+        let positionY = (((lastLoadedElements - 6) / elementsPerRow) * 320) - window.innerHeight;
+
+        if(positionY <= scrollYPosition){
+            getNextPokemon();
+        }
+    }
+}
 // #endregion
 
 // #region get data from api
@@ -32,15 +44,24 @@ async function renderMainCard(responseData={}, lastElement=bool){
     const URL = responseData.url;
     const response = await fetch(URL);
     let responseToJson = await response.json();    
-    pokemon.push({id: responseToJson.id, name: responseToJson.name, mainImage: responseToJson.sprites.other.home.front_default, types: [responseToJson.types], weight: responseToJson.weight, stats: responseToJson.stats});
-    loadDone = lastElement;
-    if(loadDone){
-        sortPokemonById();
-    }
+    if(pokemon.findIndex(element => element.id === responseToJson.id) === -1){    
+        pokemon.push({id: responseToJson.id, name: responseToJson.name, mainImage: responseToJson.sprites.other.home.front_default, types: [responseToJson.types], weight: responseToJson.weight, stats: responseToJson.stats});
+        loadDone = lastElement;
+        lastLoadedElements ++;
+        if(loadDone){
+            sortPokemonById();
+        }}
 }
 
 function sortPokemonById(){
-    pokemon.sort((a, b) => a.id - b.id);
+    pokemon.slice().sort((a, b) => a.id - b.id);
+    for(let i = 0; i < pokemon.length; i++)
+    {
+        if(i + 1 < pokemon.length){
+        if(pokemon[i + 1].id === pokemon[i].id){
+            pokemon.slice(i + 1, 1);
+        }}
+    }
     pokemonDataBase = pokemonDataBase.concat(pokemon);
     pokemon = [];
     renderCardsOnPage();
@@ -50,6 +71,7 @@ function sortPokemonById(){
 // #region render cards
 async function renderCardsOnPage(){
     const BODY_ELEMENT = document.getElementById('card_content'); 
+    BODY_ELEMENT.innerHTML = "";
     await pokemonDataBase.forEach(actualPokemon => {
             let pokemonClasses = "";
             actualPokemon.types[0].forEach(classes => {
@@ -70,8 +92,7 @@ async function Test() {
 }
 
 async function FetchNew(urlIn){
-    const response = await fetch(urlIn);
-    let nasw = await response.json();
-    console.log(nasw);
-    
+    // const response = await fetch(urlIn);
+    // let nasw = await response.json();
+    // console.log(nasw);    
 }
