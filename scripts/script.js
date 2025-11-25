62b06ad4bad3789464ee42c9301b7a094d1f041a
Dominik Koniorczyk <d.koniorczyk@limesgames.de>
2025-11-25 13:24:38 +0100

    adding dialog styling and content

--- scripts/script.js
+++ scripts/script.js
@@ -26,22 +26,22 @@ async function getNextPokemon(){
         let lastEl = responseToJson.results.findLast((element) => element) == result;
         renderMainCard(result, lastEl);
     });
+
+    let test = await fetch("https://pokeapi.co/api/v2/evolution-chain/1");
+    let testJSON = await test.json();
+    test2.push(testJSON)
 }
 
 async function renderMainCard(responseData={}, lastElement=bool){
     const URL = responseData.url;
     const response = await fetch(URL);
     let responseToJson = await response.json();    
-    if(pokemon.findIndex(element => element.id === responseToJson.id) === -1){    
-        pokemon.push({id: responseToJson.id, name: responseToJson.name, mainImage: responseToJson.sprites.other.home.front_default, types: [responseToJson.types], weight: responseToJson.weight, stats: responseToJson.stats});
+    if(pokemon.findIndex(element => element.id === responseToJson.id) === -1){  
+        pokemon.push({id: responseToJson.id, name: responseToJson.name.charAt(0).toUpperCase() + responseToJson.name.slice(1), nameLowerCase: responseToJson.name, mainImage: responseToJson.sprites.other.home.front_default, types: [responseToJson.types], weight: responseToJson.weight, stats: responseToJson.stats});
         loadDone = lastElement;
         if(loadDone){
             sortPokemonById();
-        }}
-
-        if(!test2.includes(responseToJson.types[0].type.name)){        
-            test2.push(responseToJson.types[0].type.name);}
-        
+        }};
 }
 
 function sortPokemonById(){
@@ -57,7 +57,6 @@ function sortPokemonById(){
 }
 // #endregion
 
-// #region render cards
 async function renderNextCards(){
     const BODY_ELEMENT = document.getElementById('card_content');
     let end = renderedPokemon + 50;
@@ -69,12 +68,21 @@ async function renderNextCards(){
                 pokemonClasses += returnClassImages(classes);   
             });          
         }
-        BODY_ELEMENT.innerHTML += returnCardTemplate(pokemon[i], pokemonClasses);        
+        BODY_ELEMENT.innerHTML += returnCardTemplate(pokemon[i], pokemonClasses, i, returnImagePath(pokemon[i]));        
     }
 }
 
+function returnImagePath(pokemon){
+    if(pokemon.mainImage != null){
+        return pokemon.mainImage;
+    } else {
+        return "./assets/img/question.png";
+    }
+}
+
+// #region search functions
 async function searchForPokemon(searchString){
-    pokemonToRender = pokemon.filter(pokemon => pokemon.id == parseInt(searchString) || pokemon.name.includes(searchString));
+    pokemonToRender = pokemon.filter(pokemon => pokemon.id == parseInt(searchString) || pokemon.name.includes(searchString) || pokemon.nameLowerCase.includes(searchString));
 
     if(searchString.length > 0){
         renderSearchPokemon();
@@ -95,6 +103,27 @@ async function renderSearchPokemon(){
                 pokemonClasses += returnClassImages(classes);   
             });          
         }
-        BODY_ELEMENT.innerHTML += returnCardTemplate(pokemonToRender[i], pokemonClasses);        
+        BODY_ELEMENT.innerHTML += returnCardTemplate(pokemonToRender[i], pokemonClasses, i, returnImagePath(pokemonToRender[i]));        
     }
+}
+// #endregion
+
+function openDialog(id){
+    console.log("Klick to open: " + pokemon[id].name);
+    const DIALOG_REF = document.getElementById("details");
+    DIALOG_REF.innerHTML = /*html*/`
+        <div class="dialog_body">
+            <header class="dialog_header">
+                    <h2>#${pokemon[id].id}</h2>
+                    <h2 class="dialog_inner_headline">${pokemon[id].name}</h2>
+            </header>
+            <div class="dialog_main_img ${pokemon[id].types[0][0].type.name}">
+                <img src="${returnImagePath(pokemon[id])}" alt="" srcset="">
+            </div>
+            <div class="dialog_classes">
+                ${returnClassImages(pokemon[id].types[0][0])}
+            </div>
+        </div>
+    `    
+    DIALOG_REF.showModal();
 }
--- scripts/templates.js
+++ scripts/templates.js
@@ -1,13 +1,13 @@
-function returnCardTemplate(pokemonData={}, classes) {
+function returnCardTemplate(pokemonData={}, classes, id, imgPath) {
     return /*html*/`
-        <article class = "card">
+        <article class = "card" onclick="openDialog(${id})">
             <div class = "card_inner">
                 <div class = "card_headline">
                     <h2 class = "inner_headline">#${pokemonData.id}</h2>
-                    <h2 class = "inner_headline">${pokemonData.name.charAt(0).toUpperCase() + pokemonData.name.slice(1)}</h2>
+                    <h2 class = "inner_headline">${pokemonData.name}</h2>
                 </div>
                 <div class="pokemon_img_container ${pokemonData.types[0][0].type.name}">
-                    <img src=${pokemonData.mainImage} alt="" srcset="">
+                    <img src=${imgPath} alt="" srcset="">
                 </div>
                 <div class="pokemon_type_container">
                     ${classes}
@@ -20,7 +20,9 @@ function returnCardTemplate(pokemonData={}, classes) {
 function returnClassImages(data){
     return /*html*/`
         <button class = "class_icon ${data.type.name}">
-            <div class ="${data.type.name}_image type_img" src="" alt="">
+            <div class ="${data.type.name}_image type_img" src="" alt=""> 
+                <span class="tooltiptext">${data.type.name}</span>
+            </div>
         </button>
     `;
 }
